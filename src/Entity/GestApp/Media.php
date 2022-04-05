23a934d1b0e45784d79f651c285018446ea1f3f2
Xavier BURKE <xavier.burke@openpixl.fr>
2022-04-05 07:52:50 +0200

    Création entity "Média"

---
+++ src/Entity/GestApp/Media.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity\GestApp;
+
+use App\Repository\GestApp\MediaRepository;
+use Cocur\Slugify\Slugify;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MediaRepository::class)]
+class Media
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 80)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 80)]
+    private $slug;
+
+    /**
+     * Permet d'initialiser le slug !
+     * Utilisation de slugify pour transformer une chaine de caractères en slug
+     */
+    #[ORM\PrePersist]
+    #[ORM\PreUpdate]
+    public function initializeSlug() {
+        $slugify = new Slugify();
+        $this->slug = $slugify->slugify($this->firstName."_".$this->lastName);
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
