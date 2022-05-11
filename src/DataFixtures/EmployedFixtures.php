72e7de76f7740137b2cd2f19ef21ff9c4d2fc27b
Xavier BURKE <xavier.burke@openpixl.fr>
2022-05-11 19:22:26 +0200

    Mise a jour fixture

--- src/DataFixtures/EmployedFixtures.php
+++ src/DataFixtures/EmployedFixtures.php
@@ -29,7 +29,7 @@ class EmployedFixtures extends Fixture
         $employed = new Employed();
         $employed->setRoles(array('ROLE_ADMIN'));
         $employed->setPassword($this->passwordEncoder->hashPassword($employed, 'admin123'));
-        $employed->setEmail('contact@openpixl.fr.fr');
+        $employed->setEmail('contact@openpixl.fr');
         $employed->setFirstName('admin');
         $employed->setLastName('dev');
         $employed->setIsVerified(1);
