93538fdc46be68326dc64ddd653301aca72d377b
Corwin40 <xavier.burke@openpixl.fr>
2023-01-09 18:37:35 +0100

    DEBUG

--- src/Controller/Admin/PdfController.php
+++ src/Controller/Admin/PdfController.php
@@ -32,7 +32,7 @@ class PdfController extends AbstractController
     #[Route('/admin/pdf/Property/fiche/{id}', name: 'op_admin_pdf_property', methods: ['GET'])]
     public function FicheProperty(Property $property, PropertyRepository $propertyRepository, ApplicationRepository $applicationRepository, Pdf $knpSnappyPdf, PhotoRepository $photoRepository)
     {
-        $html = 1; // variable pour basculer du mode pdf au mode html
+        $html = 0; // variable pour basculer du mode pdf au mode html
         $oneproperty = $propertyRepository->oneProperty($property->getId());
         //dd($oneproperty);
         $options = $property->getOptions();
