fcfcb248506055f769fc528bc53d827f2618ce8d
Corwin40 <xavier.burke@openpixl.fr>
2024-04-25 07:05:31 +0200

    DEBUG | Flatpickr

--- assets/admin.js
+++ assets/admin.js
@@ -6,6 +6,7 @@ import flatpickr from "flatpickr";
 import { french } from "flatpickr/dist/l10n/fr.js";
 // mise en place du datapicker flatpickr sur les champs de date
 flatpickr(".flatpickr", {
+    "locale": "fr",
     enableTime: false,
     allowInput: true,
     altFormat: "j F Y",
@@ -13,11 +14,13 @@ flatpickr(".flatpickr", {
 });
 // mise en place du datapicker flatpickr sur les champs de date
 flatpickr(".flatpickrtime", {
+    "locale": "fr",
     enableTime: true,
     noCalendar: true,
     dateFormat: "H:i",
     time_24hr: true
 });
+
 
 // start the Stimulus application
 import './bootstrap';
--- public/js/transaction/index.js
+++ public/js/transaction/index.js
@@ -1,20 +1,3 @@
-// mise en place du datapicker flatpickr sur les champs de date
-flatpickr(".flatpickr", {
-    "locale": "fr",
-    enableTime: false,
-    allowInput: true,
-    altFormat: "j F Y",
-    dateFormat: "d/m/Y",
-});
-// mise en place du datapicker flatpickr sur les champs de date
-flatpickr(".flatpickrtime", {
-    "locale": "fr",
-    enableTime: true,
-    noCalendar: true,
-    dateFormat: "H:i",
-    time_24hr: true
-});
-
 const modalCustomer = document.getElementById('modalCustomer');
 const modalDelCustomer = document.getElementById('modalDelCustomer');
 const modalAddcollaborateur = document.getElementById('modalAddColl');
