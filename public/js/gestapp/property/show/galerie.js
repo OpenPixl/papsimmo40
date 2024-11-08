b6699364cc4f1d74eb2a14ccc28b56a2a5c00267
Corwin40 <xavier.burke@openpixl.fr>
2024-11-08 09:50:42 +0100

    DEV | Galerie - video - Suppression

--- public/js/gestapp/property/show/galerie.js
+++ public/js/gestapp/property/show/galerie.js
@@ -3,7 +3,9 @@ const btndelVideo = document.querySelector('.delVideo');
 if (btndelVideo !== null) {
     btndelVideo.addEventListener('click', (e) => {
         e.preventDefault();
-        let url = this.href;
+        let a = e.currentTarget;
+        let url = a.href;
+        console.log(url);
         axios
             .post(url)
             .then(function(response){
