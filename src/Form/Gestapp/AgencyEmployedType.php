a6790caef9e494542d377edb6e3f752db43807cc
Corwin40 <xavier.burke@openpixl.fr>
2024-11-08 08:54:09 +0100

    DEV | Modification template - Video et Agents Extérieurs

--- src/Form/Gestapp/AgencyEmployedType.php
+++ src/Form/Gestapp/AgencyEmployedType.php
@@ -7,6 +7,7 @@ use App\Entity\Gestapp\AgencyEmployed;
 use App\Entity\Gestapp\Transaction;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,9 +16,14 @@ class AgencyEmployedType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+            ])
             ->add('refAgency', EntityType::class, [
+                'label' => 'Agence',
                 'class' => Agency::class,
                 'choice_label' => 'name',
             ])
