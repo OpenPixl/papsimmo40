42cf33480604a833d607ebbb7f2b85efac3e119e
Corwin40 <xavier.burke@openpixl.fr>
2025-03-12 10:57:42 +0100

    DEV | Réécriture du CSS pour les Pdf d'annonces

--- src/Controller/Admin/PdfController.php
+++ src/Controller/Admin/PdfController.php
@@ -53,7 +53,7 @@ class PdfController extends AbstractController
         return $text;
     }
 
-    #[Route('/admin/pdf/Property/fiche/{id}', name: 'op_admin_pdf_property', methods: ['GET'])]
+    #[Route('/admin/pdf/Property/fiche/client/{id}', name: 'op_admin_pdf_property', methods: ['GET'])]
     public function FicheProperty(Property $property, PropertyRepository $propertyRepository, ApplicationRepository $applicationRepository, Pdf $knpSnappyPdf, PhotoRepository $photoRepository)
     {
         $oneproperty = $propertyRepository->oneProperty($property->getId());
