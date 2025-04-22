d971067324a3c81c421ae2dbee162e1c426d26e5
Corwin40 <xavier.burke@openpixl.fr>
2025-04-22 17:57:21 +0200

    transfert

--- public/js/gestapp/transaction/show/index.js
+++ public/js/gestapp/transaction/show/index.js
@@ -78,17 +78,24 @@ if(btnHonorairePdf !== null){btnHonorairePdf.addEventListener('click', submitHon
 
 // PARTIE Codepostal sur création & modification du client
 // ---------------------------------------
-let commune2 = document.getElementById('customer_city');
-let zipcode2 = document.getElementById('customer_zipcode');
-let SelectCity2 = document.getElementById('customer_selectcity');
-let cp = '';
-let ville = '';
-if(zipcode2 !== null) {
-    zipcode2.addEventListener('input', function(event){
-        zipcode_api(zipcode2, commune2, SelectCity2, ville, cp);
+let commune = document.getElementById('customer_city');
+let zipcode = document.getElementById('customer_zipcode');
+let selectcity = document.getElementById('customer_selectcity');
+let proCity = document.getElementById('customer_proCity');
+let proZipcode = document.getElementById('customer_proZipcode');
+let proSelectcity = document.getElementById('customer_proSelectcity');
+if(zipcode !== null) {
+    zipcode.addEventListener('input', function(event){
+        zipcode_api(zipcode, commune, selectcity);
     });
-    SelectCity2.addEventListener('change', function (event){
-        change_selectcity(zipcode2, commune2, SelectCity2, ville, cp);
+    selectcity.addEventListener('change', function (event){
+        change_selectcity(zipcode, commune, selectcity);
+    });
+    proZipcode.addEventListener('input', function(event){
+        zipcode_api(proZipcode, proCity, selectcity);
+    });
+    proSelectcity.addEventListener('change', function (event){
+        change_selectcity(proZipcode, proCity, proSelectcity);
     });
 }
 
@@ -99,7 +106,7 @@ function removeOptions(selectElement) {
     }
 }
 
-function zipcode_api(zipcode, commune, select_city, ville, cp){
+function zipcode_api(zipcode, commune, select){
     if(zipcode.value.length === 5)
     {
         let coord = zipcode.value;
@@ -107,28 +114,38 @@ function zipcode_api(zipcode, commune, select_city, ville, cp){
             .get('https://apicarto.ign.fr/api/codes-postaux/communes/'+ coord)
             .then(function(response){
                 let features = response.data;
-                removeOptions(select_city);
+                removeOptions(select);
+                let ville = '';
+                let cp = '';
                 features.forEach((element) => {
-                    cp = element['codePostal'];
-                    ville = element['nomCommune'];
-                    let OptSelectCity = new Option (ville.toUpperCase()+" ("+cp+")", ville.toUpperCase(), false, true);
-                    select_city.options.add(OptSelectCity);
+                    let name =  element.nomCommune + " (" + element.codePostal + ')';
+                    ville = element.nomCommune;
+                    cp = element.codePostal;
+                    let OptSelect = new Option(name.toUpperCase(), name.toUpperCase(), false, true);
+                    select.options.add(OptSelect);
                 });
 
-                if (select_city.options.length === 1){
+                if (select.options.length === 1) {
                     zipcode.value = cp;
                     commune.value = ville.toUpperCase();
-                }else{
+                } else {
                     zipcode.value = cp;
-                    commune.value = ville.value.toUpperCase();
+                    commune.value = ville.toUpperCase();
                 }
-            });
+            })
+            .catch(function(error){
+                alert('pas de commune sur ce code postal');
+            })
+        ;
     }
 }
 
-function change_selectcity(zipcode, commune, select_city, ville, cp){
-    zipcode.value = cp;
-    commune.value = select_city.value.toUpperCase();
+function change_selectcity(zipcode, commune, select){
+    let regex = /^(.+) \((\d+)\)$/;
+    let select_value = select.options[select.selectedIndex].text;
+    const match = select_value.match(regex);
+    zipcode.value = match[2];
+    commune.value = match[1].toUpperCase();
 }
 
 function tomSelect(selectId){
@@ -140,7 +157,6 @@ function tomSelect(selectId){
         searchField: 'text',      // Champ utilisé pour la recherche
         load: function(query, callback) {
             var url = "/gestapp/customer/getCustomer";  // URL du contrôleur Symfony
-
             fetch(url)
                 .then(function(response) {
                     if (!response.ok) {
@@ -245,19 +261,17 @@ modalCustomer.addEventListener('show.bs.modal', function (event){
                 let proCity = document.getElementById('customer_proCity');
                 let proZipcode = document.getElementById('customer_proZipcode');
                 let proSelectcity = document.getElementById('customer_proSelectcity');
-                let ville = "";
-                let cp = '';
                 zipcode.addEventListener('input', function(event){
-                    zipcode_api(zipcode, commune, selectcity, ville, cp);
+                    zipcode_api(zipcode, commune, selectcity);
                 });
                 selectcity.addEventListener('change', function (event){
-                    change_selectcity(zipcode, commune, selectcity, ville, cp);
+                    change_selectcity(zipcode, commune, selectcity);
                 });
                 proZipcode.addEventListener('input', function(event){
-                    zipcode_api(zipcode, commune, selectcity, ville, cp);
+                    zipcode_api(proZipcode, proCity, proSelectcity);
                 });
                 proSelectcity.addEventListener('change', function (event){
-                    change_selectcity(zipcode, commune, selectcity, ville, cp);
+                    change_selectcity(proZipcode, proCity, proSelectcity);
                 });
             })
             .catch(function(error){
@@ -270,7 +284,6 @@ modalCustomer.addEventListener('show.bs.modal', function (event){
 modalCustomer.addEventListener('hidden.bs.modal', event => {
     let form = modalCustomer.querySelector('#formCustomer_add');
     let btnSubmit = modalCustomer.querySelector('.modal-footer #btnSubmitCustomer');
-    console.log(form);
     if(form && !btnSubmit){
         let idCustomer = document.getElementById('idCustomer').value;
         axios.post('/gestapp/customer/'+ idCustomer +'/delontransaction');
