7c178ce4997ccf771f5ca5c5eff1a95cc5d37001
Corwin40 <xavier.burke@openpixl.fr>
2025-05-02 11:04:45 +0200

    DEV | Customer - Ajout des recheches de biens

--- src/Entity/Gestapp/Customer/ResearchBien.php
+++ src/Entity/Gestapp/Customer/ResearchBien.php
@@ -77,4 +77,8 @@ class ResearchBien
 
         return $this;
     }
+
+    public function __toString(){
+        return $this->name;
+    }
 }
--- src/Entity/Gestapp/Customer/ResearchOptions.php
+++ src/Entity/Gestapp/Customer/ResearchOptions.php
@@ -74,4 +74,8 @@ class ResearchOptions
 
         return $this;
     }
+
+    public function __toString(){
+        return $this->name;
+    }
 }
